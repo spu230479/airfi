0f40f9926d672a89fa16b4a8c48f307da5b9dd06
spu230479 <spu230479@icloud.com>
2025-02-17 12:43:02 +0000

    update

--- finances-master/js/darwiniumdemo.js
+++ finances-master/js/darwiniumdemo.js
@@ -1,9 +1,10 @@
-function dwnSubmit() {
+function dwnSubmit(method) {
   const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Access-Control-Allow-Origin", "*");
 myHeaders.append("Access-Control-Allow-Origin", "GET, POST, DELETE, OPTIONS");
 
+console.log(method);
 //const raw = JSON.stringify({
 //"Email": "demouser1",
 //"password": "password",
@@ -16,10 +17,11 @@ mode: 'no-cors'
 //body: raw
 };
 
-//email = document.querySelector('input[name="Email"]').value;
-//password = document.querySelector('input[name="Password"]').value;
-email = "test@test.com";
-password = "password";
+email = document.querySelector('input[id="username"]').value;
+password = document.querySelector('input[id="password"]').value;
+localStorage.setItem("username", email);
+//email = "test@test.com";
+//password = "password";
 urlStr = "method=account_login_success&Email=" + email + "&Password=" + password;
 
 //const response = await fetch("https://airfi.tech/casino/service?" + urlStr, requestOptions);    
@@ -32,7 +34,7 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        
        console.log(xhttp.responseText);
-       document.location('./home.html');
+       window.location.replace('https://airfi.tech/finances-master/home.html');
     }
 };
 xhttp.open("POST", "https://airfi.tech/finances-master/submit?" + urlStr, false);
