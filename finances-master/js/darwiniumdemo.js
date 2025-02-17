07ceeca7bf91c62218b9ae0fb09fc75290414fd2
spu230479 <spu230479@icloud.com>
2025-02-17 14:49:43 +0000

    update

--- finances-master/js/darwiniumdemo.js
+++ finances-master/js/darwiniumdemo.js
@@ -22,6 +22,12 @@ if (method === 'login')
     localStorage.setItem("username", email);
     urlStr = "method=account_login_success&Email=" + email + "&Password=" + password;
 }
+if (method === 'otp')
+    {
+        otp = document.querySelector('input[id="otp"]').value;
+        email = localStorage.getItem("username");
+        urlStr = "method=auth_challenge_pass&otp=" + otp + "&Email=" + email;
+    }
 if (method === 'register')
 {
     fname = document.querySelector('input[id="fname"]').value;
@@ -31,6 +37,16 @@ if (method === 'register')
     mobileNumber = document.querySelector('input[id="mobileNumber"]').value;
     localStorage.setItem("username", email);
     urlStr = "method=account_creation&Email=" + email + "&mobileNumber=" + mobileNumber + "&dob=" + dob+ "&lname=" + lname+ "&fname=" + fname;
+}
+if (method === 'recover')
+{
+        fname = document.querySelector('input[id="fname"]').value;
+        lname = document.querySelector('input[id="lname"]').value;
+        dob = document.querySelector('input[id="DOB"]').value;
+        email = document.querySelector('input[id="email"]').value;
+        mobileNumber = document.querySelector('input[id="mobileNumber"]').value;
+        localStorage.setItem("username", email);
+        urlStr = "method=account_forgot_user_id&Email=" + email + "&mobileNumber=" + mobileNumber + "&dob=" + dob+ "&lname=" + lname+ "&fname=" + fname;
 }
 
 //const response = await fetch("https://airfi.tech/casino/service?" + urlStr, requestOptions);    
@@ -43,11 +59,11 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        
        console.log(xhttp.responseText);
-       if (method === 'login')
+       if (method === 'login' || method === 'otp')
        {
             window.location.replace('https://airfi.tech/finances-master/home.html');
        }
-       if (method === 'register')
+       if (method === 'register' || method === 'recover')
        {
             window.location.replace('https://airfi.tech/finances-master/otp.html');
        }
